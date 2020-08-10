75f74f1f321ac074c774de32b87e7e70b50998cf
Jason Von Busch <jasonvb29@gmail.com>
2020-08-10 08:59:35 -0500

    more changes from the course

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html'
+  // ...OR...
+  // template: 
+  // "<h1>{{pageTitle}}</h1>"
+  // ...OR...
+  template:
+  `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title: string = 'Acme Product Management';
+  pageTitle: string = 'Acme Product Management';
 }
