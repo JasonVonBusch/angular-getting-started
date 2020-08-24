73b3af8983dc8f28fffe96079f40a091ebf1ba16
JasonVonBusch <jasonvb29@gmail.com>
2020-08-24 13:56:12 -0500

    updated unit tests

--- APM-Start/src/app/products/product-list.component.spec.ts
+++ APM-Start/src/app/products/product-list.component.spec.ts
@@ -23,4 +23,14 @@ describe('ProductListComponent', () => {
     const component = fixture.componentInstance;
     expect(component.pageTitle).toEqual('Product List');
   });
+
+  it(`should toggle show image button`, () => {
+    const fixture = TestBed.createComponent(ProductListComponent);
+    fixture.detectChanges();
+    const component = fixture.componentInstance;
+    spyOn(component, 'toggleImage');
+    const button = fixture.debugElement.nativeElement.querySelector('#show-image');
+    button.click();
+    expect(component.toggleImage).toHaveBeenCalled();
+  });
 });
