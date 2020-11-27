a86d45118e24d90b084296f202d1e09c4946c44b
Jason Von Busch <jason.vonbusch@nelnet.net>
2020-11-27 10:51:01 -0600

    updated to add stars for rating and emit event when clicked

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { ProductdetailComponent } from './productdetail/productdetail.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    ProductdetailComponent
+    ProductdetailComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule,
